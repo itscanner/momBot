bb374b80244e7b89f0bb3d04ffb2805da01c4be0
itsCanner <nconnor.connor1@gmail.com>
2021-01-22 19:11:45 -0600

    added cool comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,8 @@ import net.dv8tion.jda.api.JDABuilder;
 import javax.security.auth.login.LoginException;
 
 public class Main {
-
     public static void main(String[] args) {
+        // Build JDA Object
         JDABuilder jdaBuilder = JDABuilder.createDefault("ODAyMzQwMTQzOTYzOTYzNDAy.YAtzfQ.oUvbV-ZFRYxiJ17qUI0ff5ZiCHU");
         JDA jda;
         try {
